dc69d8d2c4271547cfe294d18c44e0ed3f40a6c5
Henry Luan <henryluan95@gmail.com>
2022-07-10 20:40:53 -0400

    Created functions to count characters, words and line breaks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { useState } from "react";
 import "./App.scss";
 import bg1 from "./assets/Group1.svg";
 import bg2 from "./assets/Group2.svg";
 
 function App() {
+  const [text, setText] = useState("");
+
+  //Create a function to track user's input
+  const handleTextInput = (e) => {
+    setText(e.target.value);
+  };
+
+  //Create a function to track number of word
+  const countWord = () => {
+    if (text.length === 0) {
+      return 0;
+    } else {
+      const textArray = text.match(/(\w+)/g);
+      if (textArray === null) {
+        return 0;
+      }
+      return textArray.length;
+    }
+  };
+
+  //Create a function to track line break
+  const countLineBreak = () => {
+    return (text.match(/\n/g) || []).length;
+  };
+
   return (
     <div className="App">
       <img className="background__object1" src={bg1}></img>
@@ -11,9 +37,11 @@ function App() {
         <h1 className="card__title">What The Case</h1>
         <div className="card__input-helpers input">
           <div className="input__counters">
-            <span className="input__counter">0 Chars</span>
-            <span className="input__counter">0 Word</span>
-            <span className="input__counter">0 Line-breakers</span>
+            <span className="input__counter">{text.length} Chars</span>
+            <span className="input__counter">{countWord()} Word</span>
+            <span className="input__counter">
+              {countLineBreak()} Line-breakers
+            </span>
           </div>
           <div className="input__options">
             <button className="input__option">Clear</button>
@@ -26,6 +54,7 @@ function App() {
           name="user-input"
           id="user-input"
           placeholder="Type your text here"
+          onChange={handleTextInput}
         ></textarea>
         <div className="card__cases">
           <p className="card__case">camelCase:</p>
